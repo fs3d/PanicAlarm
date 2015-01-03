8cba0a57e8633dc5f288e07ee4c2cf801dfc0155
fs3d <pete.b.andrews@gmail.com>
2015-01-03 10:05:11 +0000

    Initial code push

---
+++ app/src/main/java/com/fs3d/pete_andrews/panicalarm/InitActivity.java
@@ -0,0 +1,64 @@
+package com.fs3d.pete_andrews.panicalarm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class InitActivity extends ActionBarActivity {
+
+    // Activity Variables
+    TextView splashView;
+    TextView copyrightView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+        splashView = (TextView) findViewById(R.id.splashText);
+        copyrightView = (TextView) findViewById(R.id.copyrightText);
+        copyrightView.setText("Copyright (C) 2015 Peter Andrews\nAll rights specified under" +
+                " applicable copyright law and international\ntreaties are hereby reserved\n\n" +
+                "Unauthorised duplication, distribution, broadcast and transmission constitutes a\n" +
+                "civil and/or criminal violation and may result in prosecution under applicable laws\n" +
+                "in either your home country, the author's home country, or the\n" +
+                "United States of America, as defined in applicable copyright law.");
+        try {
+            String splashline = "Panic Alarm v" + BuildConfig.VERSION_NAME;
+            splashView.setText(splashline);
+        } catch (Exception e) {
+            String splashline = "Panic Alarm (Unable to parse version information)";
+            e.printStackTrace();
+            splashView.setText(splashline);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_init, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            // Launch Activity with Preferences Fragments
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/fs3d/pete_andrews/panicalarm/PrefsFragmentSettings.java
@@ -0,0 +1,16 @@
+package com.fs3d.pete_andrews.panicalarm;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by peteb_000 on 03/01/2015. Further edits pending
+ */
+public class PrefsFragmentSettings extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_data_sync);
+    }
+}
