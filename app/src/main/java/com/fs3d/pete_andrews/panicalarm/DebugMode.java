dcf1685569f52fc7a7aaa63ecef64fa697956ddd
fs3d <pete.b.andrews@gmail.com>
2015-01-09 12:46:13 +0000

    Broke out preference categories to separate fragments. Updated headers Updated strings Updated preference fragments Implemented dummy Debug interface

--- app/src/main/java/com/fs3d/pete_andrews/panicalarm/DebugMode.java
+++ app/src/main/java/com/fs3d/pete_andrews/panicalarm/DebugMode.java
@@ -4,16 +4,47 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class DebugMode extends ActionBarActivity {
+
+    // Reference variables for views in activity
+    TextView statusBox;
+    Button btn_repAcc, btn_repGPS, btn_repMic, btn_repCam, btn_repSMS, btn_repMail, btn_repHeadSet, btn_repPwr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug_mode);
+        // Status Box reference
+        statusBox = (TextView) findViewById(R.id.status_view);
+        // All button references
+        btn_repGPS = (Button) findViewById(R.id.btn_repgps);
+        btn_repAcc = (Button) findViewById(R.id.btn_repacc);
+        btn_repCam = (Button) findViewById(R.id.btn_repcam);
+        btn_repMic = (Button) findViewById(R.id.btn_repmic);
+        btn_repSMS = (Button) findViewById(R.id.btn_repsms);
+        btn_repMail = (Button) findViewById(R.id.btn_repmail);
+        btn_repPwr = (Button) findViewById(R.id.btn_reppwr);
+        btn_repHeadSet = (Button) findViewById(R.id.btn_rephset);
+        // Set onClickListener to handle long press on the status text.
+        statusBox.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                statusBox.setText("Cleared by long press.");
+                return false;
+            }
+        });
+        // Preference handling routines go here.
+
+        // Service Status will be picked up here.
+
     }
 
+    // Options Menu setup goes here. Currently it only has a dummy Settings menu.
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +66,11 @@ public class DebugMode extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Additional methods below e.g. onClick XML redirects go here.
+
+    public void reportGPSStatus(View v) {
+        statusBox.append("\nGPS Status Button touched.\nGPS Status is [TEST].");
     }
 }
