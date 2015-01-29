9de1a5eeaded57f3dceafe7217d8bfdf0c80cf49
fs3d <pete.b.andrews@gmail.com>
2015-01-29 06:07:25 +0000

    Added ArrayList modifier methods to import data from the DB when opening the DB for the activity.
    To-do:
    
    Finish ArrayList population
    Populate Activity with data
    Overwrite table with new data on close of activity
    Add to DB on Contact Picker data retrieval
    Implement modifiers to remove existing contacts

--- app/src/main/java/com/fs3d/pete_andrews/panicalarm/ContactListActivity.java
+++ app/src/main/java/com/fs3d/pete_andrews/panicalarm/ContactListActivity.java
@@ -1,8 +1,10 @@
 package com.fs3d.pete_andrews.panicalarm;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract.CommonDataKinds;
@@ -18,6 +20,8 @@ import java.util.ArrayList;
 public class ContactListActivity extends ActionBarActivity {
 
     public ArrayList<ArrayList<String>> SecureContactList = new ArrayList<ArrayList<String>>();
+
+    public String id, contact_id, display_name, data_category, data_value;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +31,15 @@ public class ContactListActivity extends ActionBarActivity {
          * If an Add argument is passed, process the incoming data and then return control to
          * the activity that started this one.
          **/
+        OpenDatabase();
         String args = getIntent().getStringExtra("args");
         if (args.equals("add")) {
             FetchContact();
+            commitData();
+            finish();
         } else if (args.equals("mgr")) {
             // Manage contacts from here.
+            ManageContacts();
         }
     }
 
@@ -159,10 +167,87 @@ public class ContactListActivity extends ActionBarActivity {
         }
     }
 
+    public void OpenDatabase() {
+        // Opens the DB in read-only mode to populate an ArrayList of contacts.
+        StorageHelper mHelper = new StorageHelper(this);
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+
+        String sortOrder = StorageHelper.ContactEntry.COLUMN_NAME_CONTACT_ID + " DESC";
+
+        Cursor c = db.query(StorageHelper.ContactEntry.TABLE_NAME, null, null, null, null, null, sortOrder);
+        c.moveToFirst();
+        int locindx = 0;
+        int topindx = 0;
+        String name = "";
+        String prevtype = "_NAME";
+        ArrayList<String> nums = new ArrayList<String>();
+        ArrayList<String> mails = new ArrayList<String>();
+        do {
+            String itemId = c.getString(c.getColumnIndexOrThrow(StorageHelper.ContactEntry.COLUMN_NAME_UID));
+            String con_id = c.getString(c.getColumnIndexOrThrow(StorageHelper.ContactEntry.COLUMN_NAME_CONTACT_ID));
+            String dispname = c.getString(c.getColumnIndexOrThrow(StorageHelper.ContactEntry.COLUMN_DISPLAY_NAME));
+            String dataval = c.getString(c.getColumnIndexOrThrow(StorageHelper.ContactEntry.COLUMN_DATA_FIELD));
+            String datatype = c.getString(c.getColumnIndexOrThrow(StorageHelper.ContactEntry.COLUMN_DATA_CATEGORY));
+            if (datatype.equals("NUMBER")) {
+                if (prevtype.equals("_NAME")) {
+                    nums.add("_NUMBERS");
+                    prevtype = "_NUMBERS";
+                }
+                nums.add(dataval);
+            } else if (datatype.equals("EMAIL")) {
+                if (prevtype.equals("_NUMBERS")) {
+                    mails.add("_MAILS");
+                }
+                mails.add(dataval);
+            }
+            locindx++;
+            if (!dispname.equals(name)) {
+                // Names are different, time to make a new entry.
+                name = dispname;
+                locindx = 0;
+                topindx++;
+            }
+        } while (c.moveToNext());
+    }
+
     public void AddToContactList(String contact_id, String[] numbers, String[] emails) {
         // Append the latest contact to the Array List.
+        int rec = SecureContactList.size();
         SecureContactList.add(new ArrayList<String>());
-        SecureContactList.get(0).add(contact_id);
+        SecureContactList.get(rec).add(contact_id);
+        SecureContactList.get(rec).add("_NUMBERS");
+        for (int i = 0; i < numbers.length; i++) {
+            SecureContactList.get(rec).add(numbers[i]);
+        }
+        SecureContactList.get(rec).add("_EMAILS");
+        for (int i = 0; i < emails.length; i++) {
+            SecureContactList.get(rec).add(emails[i]);
+        }
+    }
+
+    public void ManageContacts() {
+        // Contact management - This is intended to populate the Layout with the current
+        // ArrayList of contacts.
+    }
+
+    public void commitData() {
+        // This method will commit the data changes to the Database, overwriting the old one.
+        // We can do this because this activity pulls the existing DB changes and populates an
+        // ArrayList with the current data before any changes are made.
+        StorageHelper mHelper = new StorageHelper(this);
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(StorageHelper.ContactEntry.COLUMN_NAME_UID, id);
+        values.put(StorageHelper.ContactEntry.COLUMN_NAME_CONTACT_ID, contact_id);
+        values.put(StorageHelper.ContactEntry.COLUMN_DISPLAY_NAME, display_name);
+        values.put(StorageHelper.ContactEntry.COLUMN_DATA_CATEGORY, data_category);
+        values.put(StorageHelper.ContactEntry.COLUMN_DATA_FIELD, data_value);
+
+        long newRowId;
+        newRowId = db.insert(StorageHelper.ContactEntry.TABLE_NAME,
+                StorageHelper.ContactEntry.COLUMN_NAME_CONTACT_ID,
+                values);
     }
     /*
      * Below this line is the onActivityResult method to determine what to do when a system-based
@@ -208,6 +293,7 @@ public class ContactListActivity extends ActionBarActivity {
     // The last method in this class is the exit method to close the manager on user request.
 
     public void exitContactMgr(View v) {
+        commitData();
         finish();
     }
 }
--- app/src/main/java/com/fs3d/pete_andrews/panicalarm/StorageHelper.java
+++ app/src/main/java/com/fs3d/pete_andrews/panicalarm/StorageHelper.java
@@ -8,52 +8,45 @@ import android.provider.BaseColumns;
 /**
  * Created by peteb_000 on 29/01/2015.
  */
-public class StorageHelper {
+public class StorageHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "EmergencyContacts.db";
+    private static final int DATABASE_VERSION = 1;
     private static final String STORE_CONTACT_TABLE = "contact_list";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + STORE_CONTACT_TABLE + " (" +
+                    ContactEntry.COLUMN_NAME_UID + " INTEGER PRIMARY KEY, " +
+                    ContactEntry.COLUMN_NAME_CONTACT_ID + " TEXT, " +
+                    ContactEntry.COLUMN_DISPLAY_NAME + " TEXT, " +
+                    ContactEntry.COLUMN_DATA_FIELD + " TEXT, " +
+                    ContactEntry.COLUMN_DATA_CATEGORY + " TEXT);";
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + ContactEntry.TABLE_NAME;
 
-    public StorageHelper() {
-        // Deliberately empty so that it cannot be accidentally called elsewhere.
+    public StorageHelper(Context ctxt) {
+        super(ctxt, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            // Create the database if it does not exist.
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+
+    public void onUpgrade(SQLiteDatabase db, int oldV, int newV) {
+        // Currently this method does nothing, although it needs to be declared in the Helper class.
+        }
+
+    public void onOpen(SQLiteDatabase db) {
+        // More needed here, obviously.
+        }
+
     public static abstract class ContactEntry implements BaseColumns {
+
         public static final String TABLE_NAME = "contact";
         public static final String COLUMN_NAME_UID = "uid";
         public static final String COLUMN_NAME_CONTACT_ID = "contact_id";
         public static final String COLUMN_DISPLAY_NAME = "display_name";
         public static final String COLUMN_DATA_FIELD = "data";
         public static final String COLUMN_DATA_CATEGORY = "data_category";
-    }
-
-    public class StorageDbHelper extends SQLiteOpenHelper {
-        private static final String DATABASE_NAME = "EmergencyContacts.db";
-        private static final int DATABASE_VERSION = 2;
-
-        public StorageDbHelper(Context ctxt) {
-            super(ctxt, DATABASE_NAME, null, DATABASE_VERSION);
-        }
-
-        private static final String STORE_CONTACT_TABLE_CREATE =
-                "CREATE TABLE " + STORE_CONTACT_TABLE + " (" +
-                        ContactEntry.COLUMN_NAME_UID + " INTEGER PRIMARY KEY, " +
-                        ContactEntry.COLUMN_NAME_CONTACT_ID + " TEXT, " +
-                        ContactEntry.COLUMN_DISPLAY_NAME + " TEXT, " +
-                        ContactEntry.COLUMN_DATA_FIELD + " TEXT, " +
-                        ContactEntry.COLUMN_DATA_CATEGORY + " TEXT);";
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            // Create the database if it does not exist.
-            db.execSQL(STORE_CONTACT_TABLE_CREATE);
-        }
-
-        public void onUpgrade(SQLiteDatabase db, int oldV, int newV) {
-            // Currently this method does nothing, although it needs to be declared in the Helper class.
-        }
-
-        public void onOpen(SQLiteDatabase db) {
-            // More needed here, obviously.
-        }
-
-
     }
 }
