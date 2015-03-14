2083bdc111eb029700c689ee7ca7924bb31eb8dc
fs3d <pete.b.andrews@gmail.com>
2015-03-14 13:41:22 +0000

    Further SQL Checking code in place. Hopefully the error checking is robust enough to encounter all but the most extreme Contact List situations. Attempting to implement onClick behaviour for ListView. Further testing and investigation required as it appears the underlying SDK has some bugs. Service construction is next.

--- app/src/main/java/com/fs3d/pete_andrews/panicalarm/PanicService.java
+++ app/src/main/java/com/fs3d/pete_andrews/panicalarm/PanicService.java
@@ -1,17 +1,16 @@
 package com.fs3d.pete_andrews.panicalarm;
 
-import android.annotation.TargetApi;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -20,10 +19,17 @@ import android.widget.Toast;
  */
 public class PanicService extends Service {
 
+    NotificationManager nMgr;
+    int notId = 106;
+    private Context ctxt = this.getApplicationContext();
+    private int notifierGPS = 106;
+    private int notifierGeneric = 101;
     public Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
+            String title, ticker;
+            String bodytext;
             String arg = msg.getData().getString("arg");
             String content = msg.getData().getString("message");
             switch (arg) {
@@ -34,12 +40,29 @@ public class PanicService extends Service {
                     notifyToast(content, Toast.LENGTH_LONG);
                     break;
                 case "notification":
-                    String title = "GPS";
-                    notifyPanel(title, content, 0);
+                    title = msg.getData().getString("title");
+                    ticker = msg.getData().getString("ticker");
+                    bodytext = msg.getData().getString("text");
+                    notifyPanel(notifierGeneric, new String[]{title, ticker, bodytext});
+                    break;
+                case "GPSDebug":
+                    title = "GPS Coordinate Test";
+                    ticker = "GPS Coordinates updated.";
+                    bodytext = msg.getData().getString("coords");
+                    String[] textArr = bodytext.split(":");
+                    String[] composite = new String[textArr.length + 2];
+                    composite[0] = title;
+                    composite[1] = ticker;
+                    for (int i = 0; i < textArr.length; i++) {
+                        composite[i + 2] = textArr[i];
+                    }
+                    updatePanel(notifierGPS, composite);
+                    break;
             }
         }
     };
-    private Context ctxt = this.getApplicationContext();
+
+    /* The two methods below handle Toast notification requests and Notification Bar information respectively. */
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -59,19 +82,6 @@ public class PanicService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    /* The two methods below handle Toast notification requests and Notification Bar information respectively. */
-
-    public void processArguments(String... Args) {
-        int size = Args.length;
-        for (String a : Args) {
-            Log.d("PanicService (Args)", "Processing each of " + String.valueOf(size) + ": " + a);
-        }
-    }
-
-    public void notifyToast(String output, int length) {
-        Toast.makeText(ctxt, output, length).show();
-    }
-
     /* The methods below form the branching off points for the other services the app will need access to.
      * They will all spawn their own threads and broadcast their results back here via ActivityResults.
      */
@@ -87,18 +97,79 @@ public class PanicService extends Service {
      *so that we can present any information we need to the user.
      */
 
-    @TargetApi(16)
-    public void notifyPanel(String title, String output, int mode) {
-        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        Intent intent = new Intent(ctxt, PrefsActivity.class);
-        PendingIntent pIntent = PendingIntent.getActivity(ctxt, 0, intent, 0);
-        Notification mNot = new Notification.Builder(ctxt)
-                .setContentTitle(title)
-                .setContentText(output)
-                .setSound(soundUri)
-                .addAction(0, "View", pIntent)
-                .build();
-        NotificationManager notMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notMgr.notify(0, mNot);
+    public void processArguments(String... Args) {
+        int size = Args.length;
+        for (String a : Args) {
+            Log.d("PanicService (Args)", "Processing each of " + String.valueOf(size) + ": " + a);
+        }
+    }
+
+    public void notifyToast(String output, int length) {
+        Toast.makeText(ctxt, output, length).show();
+    }
+
+    public void notifyPanel(int _ID, String... args) {
+        // Process arguments to determine the type of notification information we will share.
+        String title, ticker;
+        String[] textArr;
+        try {
+            title = args[0];
+            ticker = args[1];
+            textArr = new String[args.length - 2];
+            for (int i = 2; i < args.length; i++) {
+                textArr[i - 2] = args[i];
+            }
+        } catch (IndexOutOfBoundsException e) {
+            title = "ERROR";
+            ticker = "PanicAlarm: Internal error";
+            textArr = new String[]{e.toString()};
+        }
+        // Create a new notification panel.
+        // Invoking the default notification service
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+
+        mBuilder.setContentTitle(title);
+        mBuilder.setContentText(textArr[0]);
+        mBuilder.setTicker(ticker);
+        mBuilder.setSmallIcon(R.drawable.ic_launcher);
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+
+        // Sets a title for the Inbox style big view
+        inboxStyle.setBigContentTitle(title);
+        // Moves events into the big view
+        for (int i = 0; i < textArr.length; i++) {
+            inboxStyle.addLine(textArr[i]);
+        }
+        mBuilder.setStyle(inboxStyle);
+
+        // Increase notification number every time a new notification arrives
+        mBuilder.setNumber(_ID);
+
+        // When the user presses the notification, it is auto-removed
+        mBuilder.setAutoCancel(true);
+
+        // Creates an implicit intent
+        Intent resultIntent = new Intent("com.fs3d.pete_andrews.TEL_INTENT",
+                Uri.parse("tel:123456789"));
+        resultIntent.putExtra("from", "FS3D");
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(DebugMode.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_ONE_SHOT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        nMgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        nMgr.notify(_ID, mBuilder.build());
+    }
+
+    public void updatePanel(int _ID, String... args) {
+        // Update an existing notification panel.
+    }
+
+    public void cancelNotification(int notId) {
+        String ns = Context.NOTIFICATION_SERVICE;
+        nMgr = (NotificationManager) getApplicationContext().getSystemService(ns);
+        nMgr.cancel(notId);
     }
 }
